bdd0797163a665bf8332e5ff8314adbfbb0e4ecc
William Kwok <wkwok16@uw.edu>
2019-03-12 12:15:47 -0700

    Added React router and audited fix

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import crypto from 'crypto';
+import cookies from 'browser-cookies';
+
+// Print version number
+console.log("v1.0.0");
+
+// Set up unique id 
+let id = cookies.get('id');
+if (!id) {
+    id = crypto.randomBytes(64).toString('hex');
+    cookies.set('id', id);
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
