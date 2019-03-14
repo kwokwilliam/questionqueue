6708c3cfe64e246c5b65d525e5397d584646e29d
William Kwok <wkwok16@uw.edu>
2019-03-13 22:55:14 -0700

    Updates

---
+++ precache-manifest.6a5c289f4e2eb1d9a304aecd4bfd918f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "44658b88f6964c25ad41",
+    "url": "/static/js/4.b85547f1.chunk.js"
+  },
+  {
+    "revision": "22ea3925d9ae4a9e3c57",
+    "url": "/static/css/main.17b8eaa9.chunk.css"
+  },
+  {
+    "revision": "a5463d342ff1d2a725d9",
+    "url": "/static/js/runtime~main.82185765.js"
+  },
+  {
+    "revision": "c5f954788f341b22e1974433bb972ac1",
+    "url": "/static/media/Quicksand-Light.c5f95478.ttf"
+  },
+  {
+    "revision": "7e3a957df07d87b7fca9",
+    "url": "/static/js/2.e8aa57e3.chunk.js"
+  },
+  {
+    "revision": "0d93f5d2d23fb1c183c3",
+    "url": "/static/js/3.c4080a1a.chunk.js"
+  },
+  {
+    "revision": "22ea3925d9ae4a9e3c57",
+    "url": "/static/js/main.d268969a.chunk.js"
+  },
+  {
+    "revision": "a1bfce1d4c9bb1d6a30d302b8e81aaa3",
+    "url": "/static/media/TELabMap.a1bfce1d.png"
+  },
+  {
+    "revision": "0c64233241ead44bffbec54eb9d1d164",
+    "url": "/static/media/Quicksand-Medium.0c642332.ttf"
+  },
+  {
+    "revision": "0c04462696ac0fd3e85e75415b483fdb",
+    "url": "/static/media/Quicksand-Bold.0c044626.ttf"
+  },
+  {
+    "revision": "f87b9b4f34bdbf75b5c0cf3a5a137508",
+    "url": "/static/media/Quicksand-Regular.f87b9b4f.ttf"
+  },
+  {
+    "revision": "7e3a957df07d87b7fca9",
+    "url": "/static/css/2.6dce4215.chunk.css"
+  },
+  {
+    "revision": "daac77d83a8a71601549a8fa24651c9a",
+    "url": "/index.html"
+  }
+];
